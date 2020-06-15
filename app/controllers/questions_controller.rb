74fe02d75a3a9b6dc5c66423ffe32be32c240c34
Marie-Isabelle Schonborn <marieisabelleschoenborn@gmail.com>
2020-06-15 14:53:46 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+# app/controller/questions_controller.rb
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @questions = params[:questions]
+
+  if @questions == 'I am going to work'
+  @answer = "Great!"
+  elsif @questions.include?('?')
+  @answer = "Silly question, get dressed and go to work!"
+  else
+  @answer = "I don't care, get dressed and go to work!"
+  end
+  end
+
+
+end
